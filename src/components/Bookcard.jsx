0ecfd8a8bfdb3a95a4d1492457124deef3cb8ab3
Theasfo <thea.sofie.forrisdahl@gmail.com>
2024-04-04 11:36:24 +0200

    Startet å lege til sass

--- src/components/Bookcard.jsx
+++ src/components/Bookcard.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
-const Bookcard = ({ book }) => {
-    return(
+const BookCard = ({ book }) => {
+    const { title, first_publish_year, author_name, key: bookId, cover_i, amazon_id } = book;
+    const coverImageUrl = cover_i ? `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg` : '/path/to/default/image.jpg'; 
+
+    // Amazon URL
+    const amazonSearchUrl = `https://www.amazon.com/s?k=${encodeURIComponent(title + " book")}`;
+
+    return (
         <div className="book-card">
-            <h3>{book.title}</h3>
-            <p>Forfatter: {book.author_name?.join(',')}</p>
-            <p>Første utgivelsesår: {book.first_publish_year}</p>
-            <p>Gjennomsnittlige rating: {book.avarage_rating}</p>
-            <a href={`https://www.amazon.com/s?k=${book.amazon_id}`} target="_blank" rel="noopener noreferrer">
-                Søk på Amazon</a>
+            <img src={coverImageUrl} alt={title} />
+            <h3>{title}</h3>
+            <p>Forfatter: {author_name?.join(', ')}</p>
+            <p>Første utgivelsesår: {first_publish_year}</p>
+            <a href={amazonSearchUrl} target="_blank" rel="noopener noreferrer">
+                Søk på Amazon
+            </a>
         </div>
-    )
+    );
 }
 
-export default Bookcard
+export default BookCard;
